bc05f4d1fadb4bf412f2a623ec34eebaffacbf13
nishiikatsumi <66856922+nishiikatsumi@users.noreply.github.com>
2022-02-28 04:46:45 +0900

    Initial Commit

---
+++ pw_gen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pw_gen
+//
+//  Created by 西井勝巳 on 2022/02/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pw_gen/pw_genApp.swift
@@ -0,0 +1,17 @@
+//
+//  pw_genApp.swift
+//  pw_gen
+//
+//  Created by 西井勝巳 on 2022/02/28.
+//
+
+import SwiftUI
+
+@main
+struct pw_genApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
